d2e3c2b617064c2cd36d015ac8f03a25e49622d8
Avantika Gupta <avantika22g@gmail.com>
2025-04-23 11:22:37 +0530

    added bluieprint for auth

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const formidable = require("express-formidable");
 
+// Run command `node .\index.js`
+
 /********** App Settings **********/
 const app = express();
 const HOST = "localhost";
@@ -12,14 +14,42 @@ app.use(formidable());
 /**********************************/
 
 
+/*****************/
+/* HTTP Methods */
+/* get -- when retreiving data from db */
+/* post -- when adding data to db OR when security is needed, like sending password with request */
+/* put -- when updating data in db */
+/* delete -- when deleting data from db */
+/*****************/
+
 
 /*********** Routes ***********/
 // Extended Routes
 // app.use("/emp", empRoutes);
 
+app.post("/user-auth/signup", (req, res) => {
+  res.send("<h1><i>signup ur page</i></h1>");
+});
+
+app.post("/user-auth/signin", (req, res) => {
+  res.send("<h1><i>signin ur page</i></h1>");
+});
+
+app.put("/user-auth/update", (req, res) => {
+  res.send("<h1><i>update ur password</i></h1>");
+});
+
+app.delete("/user-auth/delete", (req, res) => {
+  res.send("<h1><i>delete ur page</i></h1>");
+});
+
+app.get("/user-auth/info", (req, res) => {
+  res.send("<h1><i>info</i></h1>");
+});
+
 // Home Routes
 app.get("/", (req, res) => {
-  res.send("<h1><i>Look at how they massacared my bow</i></h1>");
+  res.send("<h1><i>Look at how they massacared my boy</i></h1>");
 });
 /*******************************/
 
