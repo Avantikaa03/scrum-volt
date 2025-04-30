7bd4225af592cb69979500395a79b09ce7d01536
AnushhkaG <anushkagupta05120222@gmail.com>
2025-04-30 16:30:18 +0530

    reinitialized frontend
    https://tailwindcss.com/docs/installation/framework-guides/nextjs

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <div>anushka</div>
+      <div>MY PROJECT</div>
+      <Button>GO</Button>
+    </>
+  );
+}
